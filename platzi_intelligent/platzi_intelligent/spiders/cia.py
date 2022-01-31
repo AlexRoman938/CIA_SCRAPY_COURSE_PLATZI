20173cfb6af4aaad2b2b8c19723eb4fa7731c7e4
Alexander <aroman.link11@gmail.com>
2022-01-31 16:05:25 -0500

    segundo commit, terminando el scrapy

--- platzi_intelligent/platzi_intelligent/spiders/cia.py
+++ platzi_intelligent/platzi_intelligent/spiders/cia.py
@@ -3,6 +3,10 @@ import scrapy
 #links: response.xpath('//a[starts-with(@href, "collection") and (parent :: h3 | parent ::h2)]/@href').getall()
 #El xpath de arriba es que agarremos las etiqeutas a que tengan una clase href que comienza con collection y tenga como nodo padre a las etiquetas h2 y h3. Asi al final agarrar el href
 
+#title : response.xpath('//h1[@class = "documentFirstHeading"]/text()') Esto es para agarrar los títulos dentro de los links
+#paragrpah : response.xpath('//div[@class = "field-item even"]//p[not(@class)]/text()')
+
+
 class SpiderCIA(scrapy.Spider):
 
     name = 'cia'
@@ -26,7 +30,19 @@ class SpiderCIA(scrapy.Spider):
 
         for link in link_declassied:
 
-            yield response.follow(link, callback = self.parse_link)
+            yield response.follow(link, callback = self.parse_link, cb_kwargs = {'url' : response.urljoin(link)})
+            
     
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        
+        link = kwargs['url']
+        title = response.xpath('//h1[@class = "documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class = "field-item even"]//p[not(@class)]/text()').get()
+
+        yield{
+
+            'url' : link,
+            'title' : title,
+            'body' : paragraph
+
+        }
